d0fdc7d8295b770fdc85d26adf7412dbef36d475
Richard Jones <itszootime@gmail.com>
2012-11-14 14:42:13 +0000

    split tests in duration formatter

--- src/test/java/org/uncertweb/util/DurationFormatterTest.java
+++ src/test/java/org/uncertweb/util/DurationFormatterTest.java
@@ -1,6 +1,9 @@
 package org.uncertweb.util;
 
-import junit.framework.Assert;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
 
@@ -8,35 +11,65 @@ public class DurationFormatterTest {
 	
 	@Test
 	public void millis() {
-		Assert.assertEquals("0.989s", DurationFormatter.format(989));
-		Assert.assertEquals("0.001s", DurationFormatter.format(1));
+		assertThat(DurationFormatter.format(1), equalTo("0.001s"));
 	}
 	
 	@Test
 	public void seconds() {
-		Assert.assertEquals("23s", DurationFormatter.format(23001));
-		Assert.assertEquals("23.01s", DurationFormatter.format(23010));
-		Assert.assertEquals("23.01s", DurationFormatter.format(23014)); // round down
-		Assert.assertEquals("23.02s", DurationFormatter.format(23015)); // round up
-		Assert.assertEquals("23.1s", DurationFormatter.format(23100));
-		Assert.assertEquals("23.25s", DurationFormatter.format(23250));
+		assertThat(DurationFormatter.format(23001), equalTo("23s"));
+	}
+	
+	@Test
+	public void secondsWithMillis() {
+		assertThat(DurationFormatter.format(23100), equalTo("23.1s"));
+	}
+	
+	@Test
+	public void secondsWithMillisRoundDown() {
+		assertThat(DurationFormatter.format(23014), equalTo("23.01s"));
+	}
+	
+	@Test
+	public void secondsWithMillisRoundUp() {
+		assertThat(DurationFormatter.format(23015), equalTo("23.02s"));
 	}
 	
 	@Test
 	public void minutes() {
-		Assert.assertEquals("2m", DurationFormatter.format(120000));
-		Assert.assertEquals("2m5s", DurationFormatter.format(125000));
-		Assert.assertEquals("2m11s", DurationFormatter.format(131050));
-		Assert.assertEquals("2m12s", DurationFormatter.format(131500)); // round up
+		assertThat(DurationFormatter.format(120000), equalTo("2m"));
+	}
+	
+	@Test
+	public void minutesWithSeconds() {
+		assertThat(DurationFormatter.format(125000), equalTo("2m5s"));
+	}
+	
+	@Test
+	public void minutesWithSecondsAndMillisRoundDown() {
+		assertThat(DurationFormatter.format(131050), equalTo("2m11s"));
+	}
+	
+	@Test
+	public void minutesWithSecondsAndMillisRoundUp() {
+		assertThat(DurationFormatter.format(131500), equalTo("2m12s"));
 	}
 	
 	@Test
 	public void hours() {
-		long minute = 1000 * 60;
-		long hour = minute * 60;
-		Assert.assertEquals("5h", DurationFormatter.format(5 * hour));
-		Assert.assertEquals("5h30m", DurationFormatter.format(5 * hour + 30 * minute));
-		Assert.assertEquals("5h7m", DurationFormatter.format(5 * hour + 7 * minute + 30000)); // shouldn't round up
+		long duration = TimeUnit.HOURS.toMillis(5);
+		assertThat(DurationFormatter.format(duration), equalTo("5h"));
+	}
+	
+	@Test
+	public void hoursWithMinutes() {
+		long duration = TimeUnit.HOURS.toMillis(5) + TimeUnit.MINUTES.toMillis(30);
+		assertThat(DurationFormatter.format(duration), equalTo("5h30m"));
+	}
+	
+	@Test
+	public void hoursWithMinutesAndSecondsNoRound() {
+		long duration = TimeUnit.HOURS.toMillis(5) + TimeUnit.MINUTES.toMillis(7) + 30000;
+		assertThat(DurationFormatter.format(duration), equalTo("5h7m"));
 	}
 
 }
