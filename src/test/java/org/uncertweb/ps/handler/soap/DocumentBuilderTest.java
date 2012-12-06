c03965f79e134535d9e5d49b0c777ea480a979b1
Richard Jones <itszootime@gmail.com>
2012-12-06 10:56:34 +0000

    some document builder tests

---
+++ src/test/java/org/uncertweb/ps/handler/soap/DocumentBuilderTest.java
@@ -0,0 +1,22 @@
+package org.uncertweb.ps.handler.soap;
+
+import org.junit.Before;
+
+public class DocumentBuilderTest {
+
+	private DocumentBuilder builder;
+	
+	@Before
+	public void before() {
+		builder = new DocumentBuilder();
+	}
+	
+	public void buildValidDocument() {
+		
+	}
+	
+	public void buildInvalidDocument() {
+		
+	}
+	
+}
