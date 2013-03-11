1bb37a73ceebcdda7b5927393180e871e6b806bd
Richard Jones <itszootime@gmail.com>
2013-03-11 16:10:49 +0000

    Remove unused file, add whitespace test on ignore

--- src/test/java/org/uncertweb/ps/handler/soap/DocumentBuilderTest.java
+++ src/test/java/org/uncertweb/ps/handler/soap/DocumentBuilderTest.java
@@ -10,6 +10,7 @@ import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.JDOMException;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.uncertweb.ps.test.RunningService;
@@ -50,6 +51,16 @@ public class DocumentBuilderTest {
 		builder.build(this.getClass().getClassLoader().getResourceAsStream("xml/sum-invalid-soaprequest.xml"));
 		ValidationResult result = builder.getValidationResult();
 		assertThat(result.isValid(), equalTo(false));
+	}
+	
+	@Test
+	@Ignore
+	// TODO: I would prefer this mechanism over the content selection in XMLRequestParser
+	public void buildStripsWhitespace() throws JDOMException, IOException {
+		Document document = builder.build(this.getClass().getClassLoader().getResourceAsStream("xml/bufferpolygon-request.xml"));
+		Element polygon = document.getRootElement().getChild("Polygon", Namespaces.PS);
+		assertThat(polygon, notNullValue());
+		assertThat(polygon.getContentSize(), equalTo(1));
 	}
 	
 }
