e179481cf80012664a6fc17a6b0a9abdf8743a3b
Richard Jones <itszootime@gmail.com>
2013-03-11 15:44:39 +0000

    Add test for whitespace stripping bug

--- src/test/java/org/uncertweb/ps/handler/soap/DocumentBuilderTest.java
+++ src/test/java/org/uncertweb/ps/handler/soap/DocumentBuilderTest.java
@@ -7,12 +7,13 @@ import static org.hamcrest.Matchers.notNullValue;
 import java.io.IOException;
 
 import org.jdom.Document;
+import org.jdom.Element;
 import org.jdom.JDOMException;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.uncertweb.ps.test.RunningService;
-
+import org.uncertweb.xml.Namespaces;
 public class DocumentBuilderTest {
 	
 	@Rule
@@ -49,6 +50,14 @@ public class DocumentBuilderTest {
 		builder.build(this.getClass().getClassLoader().getResourceAsStream("xml/sum-invalid-soaprequest.xml"));
 		ValidationResult result = builder.getValidationResult();
 		assertThat(result.isValid(), equalTo(false));
+	}
+	
+	@Test
+	public void buildStripsWhitespace() throws JDOMException, IOException {
+		Document document = builder.build(this.getClass().getClassLoader().getResourceAsStream("xml/bufferpolygon-request.xml"));
+		Element polygon = document.getRootElement().getChild("Polygon", Namespaces.PS);
+		assertThat(polygon, notNullValue());
+		assertThat(polygon.getContentSize(), equalTo(1));
 	}
 	
 }
