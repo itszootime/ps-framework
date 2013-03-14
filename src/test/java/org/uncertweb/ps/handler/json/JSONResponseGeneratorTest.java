d5dd25b3b27d76bde7dfac422c3e39887e701262
Richard Jones <itszootime@gmail.com>
2013-03-14 12:22:40 +0000

    Add the first JSONResponseGenerator tests

--- src/test/java/org/uncertweb/ps/handler/json/JSONResponseGeneratorTest.java
+++ src/test/java/org/uncertweb/ps/handler/json/JSONResponseGeneratorTest.java
@@ -1,5 +1,143 @@
 package org.uncertweb.ps.handler.json;
 
-public class JSONResponseGeneratorTest {
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.startsWith;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.jdom.Element;
+import org.junit.Rule;
+import org.junit.Test;
+import org.uncertweb.ps.data.RequestedOutput;
+import org.uncertweb.ps.data.Response;
+import org.uncertweb.ps.handler.ResponseGenerateException;
+import org.uncertweb.ps.handler.soap.XMLResponseGenerator;
+import org.uncertweb.ps.test.ConfiguredService;
+import org.uncertweb.test.util.TestData;
+import org.uncertweb.xml.Namespaces;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class JSONResponseGeneratorTest {
+	
+	@Rule
+	public ConfiguredService service = new ConfiguredService();
+	
+	private JsonObject generateResponse(Response response) {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		JSONResponseGenerator.generate(response, baos);
+		JsonParser parser = new JsonParser();
+		return parser.parse(baos.toString()).getAsJsonObject();
+	}
+
+	@Test
+	public void generateNotNull() throws ResponseGenerateException {
+		JsonObject response = generateResponse(TestData.getSumResponse());
+		assertThat(response, notNullValue());
+	}
+
+	@Test
+	public void generateProcessName() throws ResponseGenerateException {
+		JsonObject response = generateResponse(TestData.getSumResponse());
+		assertThat(response.has("SumProcessResponse"), equalTo(true));
+	}
+
+//	@Test
+//	public void generateElementNamespace() throws ResponseGenerateException {
+//		// generate
+//		Element responseElement = XMLResponseGenerator.generate(TestData.getSumResponse());
+//
+//		// check
+//		assertThat(responseElement.getNamespace(), equalTo(Namespaces.PS));
+//	}
+//	
+//	@Test
+//	public void generateOutputNotNull() throws ResponseGenerateException {
+//		// generate
+//		Element responseElement = XMLResponseGenerator.generate(TestData.getSumResponse());
+//
+//		// check
+//		Element resultElement = responseElement.getChild("Result", Namespaces.PS);
+//		assertThat(resultElement, notNullValue());
+//	}
+//	
+//	@Test
+//	public void generateOutputValue() throws ResponseGenerateException {
+//		// generate
+//		Element responseElement = XMLResponseGenerator.generate(TestData.getSumResponse());
+//
+//		// check
+//		Element resultElement = responseElement.getChild("Result", Namespaces.PS);
+//		assertThat(resultElement.getText(), equalTo("101.05"));
+//	}
+//	
+//	@Test
+//	public void generateRequestedOutputsEmpty() throws ResponseGenerateException {
+//		// generate
+//		Element responseElement = XMLResponseGenerator.generate(TestData.getHashResponse(), Arrays.asList(new RequestedOutput[0]));
+//		
+//		// check
+//		int resultElementCount = responseElement.getChildren().size();
+//		assertThat(resultElementCount, equalTo(0));
+//	}
+//
+//	@Test
+//	public void generateRequestedOutputsCount() throws ResponseGenerateException {
+//		// generate
+//		Element responseElement = XMLResponseGenerator.generate(TestData.getHashResponse(), Arrays.asList(new RequestedOutput[] {
+//				new RequestedOutput("SHA1", false)
+//		}));;
+//		
+//		// check
+//		int resultElementCount = responseElement.getChildren().size();
+//		assertThat(resultElementCount, equalTo(1));
+//	}
+//	
+//	@Test
+//	public void generateRequestedOutputName() throws ResponseGenerateException {
+//		// generate
+//		Element responseElement = XMLResponseGenerator.generate(TestData.getHashResponse(), Arrays.asList(new RequestedOutput[] {
+//				new RequestedOutput("SHA1", false)
+//		}));;
+//		
+//		// check
+//		Element resultElement = responseElement.getChild("SHA1", Namespaces.PS);
+//		assertThat(resultElement, notNullValue());
+//	}
+//
+//	@Test
+//	public void generateDataReferenceSensibleURL() throws ResponseGenerateException, IOException {
+//		// setup
+//		String expectedBaseURL = service.getBaseURL() + "/data/";
+//
+//		// generate
+//		Element responseElement = XMLResponseGenerator.generate(TestData.getBufferPolygonResponse(), Arrays.asList(new RequestedOutput[] {
+//				new RequestedOutput("BufferedPolygon", true)
+//		}));
+//
+//		// check
+//		Element referenceElement = responseElement.getChild("BufferedPolygon", Namespaces.PS).getChild("DataReference", Namespaces.PS);
+//		String url = referenceElement.getAttributeValue("href");
+//		assertThat(url, startsWith(expectedBaseURL));
+//	}
+//
+//	@Test
+//	public void generateDataReferenceMimeType() throws ResponseGenerateException, IOException {
+//		// generate
+//		Element responseElement = XMLResponseGenerator.generate(TestData.getBufferPolygonResponse(), Arrays.asList(new RequestedOutput[] {
+//				new RequestedOutput("BufferedPolygon", true)
+//		}));
+//
+//		// check
+//		Element referenceElement = responseElement.getChild("BufferedPolygon", Namespaces.PS).getChild("DataReference", Namespaces.PS);
+//		String url = referenceElement.getAttributeValue("mimeType");
+//		assertThat(url, equalTo("text/xml"));
+//	}
+	
 }
